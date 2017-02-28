ba8f98301c56b24bfc0602f5bffb584143a656f9
Rob Fenlon <rob.fenlon@kneat.com>
2017-02-28 09:54:16 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+servular - A simple HTTP(S) server, aimed for single page apps
+==============================================================
+
+Small, compact node server to serve up any local directory. While this is aimed
+at single page applications, it can serve up any site big or small.
+
+Usage
+-----
+```
+npm install -g servular
+servular [options]
+```
+
+Options
+-------
+`-s` or `--ssl`: Use HTTPS (see --key and --cert)
+`-r` or `--redirect`: When using HTTPS, use this flag to redirect HTTP/80 requests to the HTTPS service
+`-k` or `--key [file]`: Path to key file (default: key.pem)
+`-c` or `--cert [file]`: Path to cert file (default: cert.pem)
+`-p` or `--port [number]`: Specify a port (default: 443 with --ssl, otherwise 80)
+`-a` or `--address [IP/Name]`: Specify an address to use (default: 127.0.0.1)
+`-v` or `--verbose-output`: Output request info to the console
+`-d` or `--default [file]`: If requested file cannot be resolved, serve the requested page instead (default: index.html)
---
+++ servular.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+'use strict'
+
+const http = require('http'),
+   https = require('https'),
+   fs = require('fs'),
+   args = require('commander'),
+   mimeTypes = require('mime-types'),
+   packageJson = require('./package.json');
+
+args
+   .version(packageJson.version)
+   .option('-s, --ssl', 'Use HTTPS (see --key and --cert)')
+   .option('-r, --redirect', 'When using HTTPS, use this flag to redirect HTTP/80 requests to the HTTPS service')
+   .option('-k, --key [file]', 'Path to key file [key.pem]', 'key.pem')
+   .option('-c, --cert [file]', 'Path to cert file [cert.pem]', 'cert.pem')
+   .option('-p, --port [number]', 'Specify a port [443 with --ssl, otherwise 80]')
+   .option('-a, --address [IP/Name]', 'Specify an address to use [127.0.0.1]', '127.0.0.1')
+   .option('-v, --verbose-output', 'Output request info to the console')
+   .option('-d, --default [file]', 'If requested file cannot be resolved, serve the requested page instead [index.html]', 'index.html')
+   .parse(process.argv);
+
+const options = args.ssl ?
+   {
+      key: fs.readFileSync(args.key).toString(),
+      cert: fs.readFileSync(args.cert).toString()
+   } :
+   {},
+   port = args.port || (args.ssl ? 443 : 80);
+
+const app = function (req, res) {
+   const serveFile = function (requestedResource) {
+      try {
+         var buffer = fs.readFileSync(requestedResource);
+      } catch (err) {
+         if (err.code === 'ENOENT') {
+            console.error(requestedResource + ' does not exist, serving ' + args.default + ' instead.');
+            return serveFile(args.default);
+         } else {
+            throw err;
+         }
+      }
+
+      return {
+         header: mimeTypes.lookup(requestedResource),
+         content: buffer
+      };
+   };
+
+   if (args.verboseOutput) {
+      const date = new Date().toUTCString();
+      console.log(date + ' - Request: ' + req.url);
+   }
+
+   const requestedResource = req.url.split('?')[0].substring(1) || args.default,
+      resource = serveFile(requestedResource);
+
+   res.writeHead(200, { 'Content-Type': resource.header });
+   res.write(resource.content);
+   res.end();
+};
+
+const server = args.ssl ? https.createServer(options, app) : http.createServer(app);
+
+server.listen(port, args.address, function () {
+   const prefix = args.ssl ? 'https://' : 'http://';
+   return console.log('servular started on ' + prefix + args.address + ':' + port);
+});
+
+if (args.ssl && args.redirect) {
+   const redirectApp = function (req, res) {
+      const host = req.headers.host.split(':')[0];
+      const redirectUrl = 'https://' + host + ':' + port + req.url;
+      if (args.verboseOutput) {
+         const date = new Date().toUTCString();
+         console.log(date + ' - Redirect to: ' + redirectUrl);
+      }
+      res.writeHead(301, { 'Location': redirectUrl });
+      res.end();
+   }
+
+   http.createServer(redirectApp).listen(80, args.address, function () {
+      return console.log('servular redirect started on http://' + args.address + ':80');
+   });
+}
